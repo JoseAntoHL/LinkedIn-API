b3ecb8018143bf261276f8f78e11a2c333f70871
JoseAntoHL <71193848+JoseAntoHL@users.noreply.github.com>
2021-11-26 00:34:11 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    'linkedinAuth': {
+        'clientID': '78utpsd1b9b7ty', // your App ID
+        'clientSecret': 'rb7hSm0VJTuH1uSX', // your App Secret
+        'callbackURL': 'http://127.0.0.1:3000/auth/linkedin/callback'
+    }
+}
